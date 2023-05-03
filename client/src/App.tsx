d8ab93f183665a2bd867fce404c5a84d9acd455f
jeyofdev <jgregoire.pro@gmail.com>
2023-05-03 16:59:58 +0200

    feat : init client app with vite and react

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import Hello from './components/Hello';
+
+const App = () => (
+	<div
+		style={{
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			minHeight: '100vh',
+		}}
+	>
+		<Hello />
+	</div>
+);
+
+export default App;
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-// function for init test
-const sum = (a: number, b: number): number => {
-	const result = a + b;
-	return result;
-};
-
-export default sum;
