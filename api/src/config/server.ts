00d7e7aff9e6afc6593a2ec1c2f1ca139f5e8ba1
jeyofdev <jgregoire.pro@gmail.com>
2023-05-08 09:52:19 +0200

    refacto : add function getServer

---
+++ api/src/config/server.ts
@@ -0,0 +1,24 @@
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { readFileSync } from 'fs';
+import { ApolloServer } from '@apollo/server';
+import { IContext, context } from '../context';
+import resolvers from '../resolvers';
+
+const getServer = async (port?: number) => {
+	const typeDefs = readFileSync('./src/schema/schema.graphql', {
+		encoding: 'utf-8',
+	});
+
+	const server = new ApolloServer<IContext>({
+		typeDefs,
+		resolvers,
+	});
+
+	const { url } = await startStandaloneServer(server, {
+		context: async () => context,
+		listen: { port },
+	});
+	return { server, url };
+};
+
+export default getServer;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,5 @@
-import { readFileSync } from 'fs';
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import resolvers from './resolvers';
+import getServer from './config/server';
 import 'dotenv/config';
-import { IContext, context } from './context';
 
 const runServer = async () => {
 	const { PORT } = process.env;
@@ -12,19 +8,7 @@ const runServer = async () => {
 		throw new Error('The environment variable PORT must be specified');
 	}
 
-	const typeDefs = readFileSync('./src/schema/schema.graphql', {
-		encoding: 'utf-8',
-	});
-
-	const server = new ApolloServer<IContext>({
-		typeDefs,
-		resolvers,
-	});
-
-	const { url } = await startStandaloneServer(server, {
-		context: async () => context,
-		listen: { port: Number(PORT) },
-	});
+	const { url } = await getServer(Number(PORT));
 
 	// eslint-disable-next-line no-console
 	console.log(`🚀  Server ready at: ${url}`);
