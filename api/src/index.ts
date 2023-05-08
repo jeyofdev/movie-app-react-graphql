fe33cb59ee615264be39275efba53a480ea35e65
jeyofdev <jgregoire.pro@gmail.com>
2023-05-08 10:41:21 +0200

    ci : add environment variable to workflows github

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,13 +2,13 @@ import getServer from './config/server';
 import 'dotenv/config';
 
 const runServer = async () => {
-	const { PORT } = process.env;
+	const { API_PORT } = process.env;
 
-	if (!PORT) {
-		throw new Error('The environment variable PORT must be specified');
+	if (!API_PORT) {
+		throw new Error('The environment variable API_PORT must be specified');
 	}
 
-	const { url } = await getServer(Number(PORT));
+	const { url } = await getServer(Number(API_PORT));
 
 	// eslint-disable-next-line no-console
 	console.log(`🚀  Server ready at: ${url}`);
