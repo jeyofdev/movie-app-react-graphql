583e8d33e5715acc428f5bef23b674e3d7693e08
jeyofdev <jgregoire.pro@gmail.com>
2023-05-16 17:12:44 +0200

    style client : fix responsive mainContainer

--- client/src/components/containers/mainContainer/style.ts
+++ client/src/components/containers/mainContainer/style.ts
@@ -14,17 +14,21 @@ const useStyles = (theme: Theme) => ({
 		[theme.breakpoints.up('sm')]: {
 			padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
 			borderRadius: '1.1rem',
+			minHeight: `calc(100vh - (2 * ${theme.spacing(3)}))`,
 		},
 		[theme.breakpoints.up('md')]: {
 			padding: `${theme.spacing(4)} ${theme.spacing(5)}`,
 			borderRadius: '1.25rem',
+			minHeight: `calc(100vh - (2 * ${theme.spacing(4)}))`,
 		},
 		[theme.breakpoints.up('lg')]: {
 			padding: `${theme.spacing(4.5)} ${theme.spacing(5)}`,
+			minHeight: `calc(100vh - (2 * ${theme.spacing(4.5)}))`,
 		},
 		[theme.breakpoints.up('xl')]: {
 			padding: `${theme.spacing(5)} ${theme.spacing(6.5)}`,
 			maxWidth: `calc(1920px - (2 * ${theme.spacing(8)}))`,
+			minHeight: `calc(100vh - (2 * ${theme.spacing(5)}))`,
 		},
 	},
 });
