a694b0ab43fce48cac8dc8365735c913e2aeff9c
jeyofdev <jgregoire.pro@gmail.com>
2023-05-22 14:28:45 +0200

    refacto : create list container

---
+++ client/src/components/containers/listContainer/style.ts
@@ -0,0 +1,41 @@
+import { Theme } from '@mui/material';
+
+const useStyles = (theme: Theme) => ({
+	root: {},
+	primaryContentBox: {
+		gridColumn: {
+			xs: 'auto / span 12',
+			sm: 'auto / span 8',
+			md: 'auto / span 9',
+			lg: 'auto / span 10',
+			xl: 'auto / span 11',
+		},
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	darkMode: {
+		alignSelf: 'flex-start',
+	},
+	cards: {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(12, 1fr)',
+		gap: {
+			xs: `${theme.spacing(4)} ${theme.spacing(2)}`,
+			sm: `${theme.spacing(4)} ${theme.spacing(3)}`,
+			md: `${theme.spacing(4)} ${theme.spacing(3)}`,
+			lg: `${theme.spacing(4)} ${theme.spacing(3)}`,
+			xl: `${theme.spacing(5)} ${theme.spacing(4)}`,
+		},
+		maxWidth: {
+			xs: '200px',
+			sm: '400px',
+			md: '600px',
+			lg: '850px',
+			xl: '1350px',
+		},
+		justifyContent: 'center',
+	},
+});
+
+export default useStyles;
