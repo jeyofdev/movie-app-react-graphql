d64ef89c337e033fda2336a312fa8313de9b78c0
jeyofdev <jgregoire.pro@gmail.com>
2023-05-27 11:39:34 +0200

    feat : sort movies results

--- api/src/resolvers/enums/index.ts
+++ api/src/resolvers/enums/index.ts
@@ -11,6 +11,14 @@ const enums = {
 		US: 'US',
 		ES: 'ES',
 	},
+	SortEnum: {
+		POPULARITY_ASC: 'popularity.asc',
+		POPULARITY_DESC: 'popularity.desc',
+		VOTE_ASC: 'vote.asc',
+		VOTE_DESC: 'vote.desc',
+		PRIMARY_RELEASE_DATE_ASC: 'primary.release.date.asc',
+		PRIMARY_RELEASE_DATE_DESC: 'primary.release.date.desc',
+	},
 };
 
 export default enums;
