75e3914c1a9d6655f45128903762b5ffcb201ae5
jeyofdev <jgregoire.pro@gmail.com>
2023-06-12 18:52:22 +0200

    fix : genre page title translation

--- client/src/pages/genre/Genre.tsx
+++ client/src/pages/genre/Genre.tsx
@@ -3,20 +3,21 @@ import ListContainer from '@components/containers/listContainer/ListContainer';
 import AlertBase from '@components/ui/alert/Alert';
 import { TranslationContext } from '@context/TranslationContext';
 import {
+	Genre as GenreType,
 	Movie,
 	useDiscoverMoviesByGenreQuery,
+	useGenresQuery,
 } from '@graphql/__generated__/graphql-type';
 import { Trans } from '@lingui/macro';
 import { Box } from '@mui/material';
 import { firstLetterCapitalize } from '@utils/index';
 import { ChangeEvent, useContext, useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import useStyles from './style';
 
 const Genre = () => {
 	const styles = useStyles();
 	const location = useLocation();
-	const { genreName } = useParams();
 	const { currentLocale } = useContext(TranslationContext);
 
 	const [page, setPage] = useState(1);
@@ -32,6 +33,19 @@ const Genre = () => {
 		fetchPolicy: 'cache-and-network',
 	});
 
+	const {
+		data: dataGenres,
+		loading: loadingGenres,
+		error: errorGenres,
+	} = useGenresQuery({
+		variables: {
+			options: {
+				language: currentLocale,
+			},
+		},
+		fetchPolicy: 'no-cache',
+	});
+
 	const handleChangePage = (e: ChangeEvent<unknown>, value: number) => {
 		setPage(value);
 	};
@@ -40,11 +54,11 @@ const Genre = () => {
 		window.scrollTo(0, 0);
 	}, []);
 
-	if (loading) {
+	if (loading || loadingGenres) {
 		return <LoaderContainer />;
 	}
 
-	if (error) {
+	if (error || errorGenres) {
 		return (
 			<AlertBase>
 				<Trans>An error has occurred !!!</Trans>
@@ -56,7 +70,11 @@ const Genre = () => {
 		<Box sx={styles.root}>
 			<ListContainer
 				list={data?.discoverMoviesByGenre?.results as Array<Movie>}
-				title={firstLetterCapitalize(genreName?.replaceAll('-', ' ') as string)}
+				title={firstLetterCapitalize(
+					(dataGenres?.genres?.genres as Array<GenreType>).find(
+						genre => genre.id === Number(location?.state?.genreId),
+					)?.name as string,
+				)}
 				totalPages={data?.discoverMoviesByGenre?.total_pages as number}
 				currentPage={page}
 				setCurrentPage={handleChangePage}
