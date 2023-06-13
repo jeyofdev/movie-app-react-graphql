d4cb458d7db90737e6eac8a36d35a5b0a48d44ca
jeyofdev <jgregoire.pro@gmail.com>
2023-06-13 13:09:21 +0200

    feat : show modal in landing page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,20 +1,34 @@
 import HomeCard from '@components/cards/homeCard/HomeCard';
 import LoaderContainer from '@components/containers/LoaderContainer/LoaderContainer';
+import SignInModal from '@components/modals/signInModal/SignInModal';
+import SignUpModal from '@components/modals/signUpModal/SignUpModal';
 import AlertBase from '@components/ui/alert/Alert';
 import { TranslationContext } from '@context/TranslationContext';
 import {
 	Movie,
 	usePopularMoviesQuery,
 } from '@graphql/__generated__/graphql-type';
-import { Trans } from '@lingui/macro';
-import { Box } from '@mui/material';
+import { Trans, t } from '@lingui/macro';
+import { Box, Button, Typography, useTheme } from '@mui/material';
+import { auth } from '@services/firebase';
 import { useContext, useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { RoutesEnum } from '../../types/enums';
 import useStyles from './style';
 
 const Home = () => {
-	const styles = useStyles();
+	const theme = useTheme();
+	const styles = useStyles(theme);
+	const navigate = useNavigate();
+	const [user] = useAuthState(auth);
 	const { currentLocale } = useContext(TranslationContext);
+
 	const [moviesList, setMoviesList] = useState<Array<Movie>>([]);
+	const [showModalSignUp, setShowModalSignUp] = useState<boolean>(false);
+	const [showModalSignIn, setShowModalSignIn] = useState<boolean>(false);
+	const [signInStep, setSignInStep] = useState<number>(0);
+	const [logInStep, setLogInStep] = useState<number>(0);
 
 	const { loading, error } = usePopularMoviesQuery({
 		variables: {
@@ -76,7 +90,85 @@ const Home = () => {
 						/>
 					))}
 				</Box>
+
+				<Box sx={styles.btnsBox}>
+					{!user ? (
+						<>
+							<Button
+								variant='outlined'
+								disableFocusRipple
+								disableRipple
+								sx={styles.btn}
+								onClick={() => {
+									setShowModalSignIn(true);
+									setLogInStep(1);
+								}}
+							>
+								<Typography variant='h6'>
+									<Trans>Login</Trans>
+								</Typography>
+							</Button>
+
+							<Button
+								variant='contained'
+								disableFocusRipple
+								disableRipple
+								sx={{ ...styles.btn, ...styles.btnContained }}
+								onClick={() => {
+									setShowModalSignUp(true);
+									setSignInStep(1);
+								}}
+							>
+								<Typography variant='h6'>
+									<Trans>Sign up</Trans>
+								</Typography>
+							</Button>
+						</>
+					) : (
+						<Button
+							variant='contained'
+							sx={{ ...styles.btn, ...styles.btnContained }}
+							disableFocusRipple
+							disableRipple
+							onClick={() => navigate(RoutesEnum.MOVIES_LIST)}
+						>
+							<Typography variant='h6'>
+								<Trans>Getting started</Trans>
+							</Typography>
+						</Button>
+					)}
+				</Box>
 			</Box>
+
+			<SignUpModal
+				open={showModalSignUp}
+				setOpen={setShowModalSignUp}
+				step={signInStep}
+				setStep={setSignInStep}
+				title={{
+					stepOne: t`Sign up`,
+					stepTwo: t`Finish Signing up`,
+				}}
+				onRedirect={() => {
+					setShowModalSignIn(true);
+					setShowModalSignUp(false);
+				}}
+			/>
+
+			<SignInModal
+				open={showModalSignIn}
+				setOpen={setShowModalSignIn}
+				step={logInStep}
+				setStep={setLogInStep}
+				title={{
+					stepOne: t`Login`,
+					stepTwo: t`Forgot your password ?`,
+				}}
+				onRedirect={() => {
+					setShowModalSignUp(true);
+					setShowModalSignIn(false);
+				}}
+			/>
 		</Box>
 	);
 };
--- client/src/pages/home/style.ts
+++ client/src/pages/home/style.ts
@@ -1,4 +1,6 @@
-const useStyles = () => ({
+import { Theme } from '@mui/material';
+
+const useStyles = (theme: Theme) => ({
 	root: {},
 	imagesBox: {
 		display: 'flex',
@@ -31,6 +33,37 @@ const useStyles = () => ({
 			lg: 'rotate(18deg) translate(16px, 23px)',
 		},
 		zIndex: 10,
+	},
+	btnsBox: {
+		position: 'relative',
+		display: 'flex',
+		justifyContent: 'center',
+		gap: theme.spacing(2),
+		// background: 'red',
+		zIndex: 1001,
+		marginTop: {
+			xs: '230px',
+			sm: '330px',
+			md: '330px',
+			lg: '450px',
+			xl: '450',
+		},
+	},
+	btn: {
+		borderRadius: '100px',
+		padding: `${theme.spacing(1)} ${theme.spacing(4)}`,
+		border: `2px solid ${theme.palette.primary.main}`,
+
+		'&:hover': {
+			border: `2px solid ${theme.palette.primary.main}`,
+		},
+	},
+	btnContained: {
+		backgroundColor: theme.palette.primary.main,
+
+		'&:hover': {
+			backgroundColor: theme.palette.primary.main,
+		},
 	},
 });
 
